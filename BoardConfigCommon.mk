fd69778febd052d9cc344b6aab40e089ceafbb66
Dylan Neve <dylanneve1@gmail.com>
2022-06-01 00:54:23 +0100

    universal8895-common: mkbootimg: Import from samsung/hardware
    * All credits go to LineageOS team

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,7 +64,7 @@ TARGET_USES_64_BIT_BINDER := true
 
 # Extracted with libbootimg
 BOARD_CUSTOM_BOOTIMG := true
-BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
+BOARD_CUSTOM_BOOTIMG_MK := $(COMMON_PATH)/mkbootimg/mkbootimg.mk
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
